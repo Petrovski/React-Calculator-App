3e5d30aaed6ea3a1b50214530752e6a6eff588c5
FORTRESS <alexpetroski@gmail.com>
2019-07-25 10:09:29 -0400

    Updates

---
+++ precache-manifest.0cf41d8b89d123545cdfd07f57513543.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30656a3036838554b51287d6e6fb6609",
+    "url": "/React-Calculator-App/index.html"
+  },
+  {
+    "revision": "6fe99b6cce3a4ecec69d",
+    "url": "/React-Calculator-App/static/css/main.5a289802.chunk.css"
+  },
+  {
+    "revision": "c091e4d9c9d22d64c3c9",
+    "url": "/React-Calculator-App/static/js/2.70dee73b.chunk.js"
+  },
+  {
+    "revision": "6fe99b6cce3a4ecec69d",
+    "url": "/React-Calculator-App/static/js/main.a569c233.chunk.js"
+  },
+  {
+    "revision": "6a1652b39af3051cd4c6",
+    "url": "/React-Calculator-App/static/js/runtime~main.98711bb7.js"
+  }
+]);
